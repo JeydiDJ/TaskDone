5d7fdb3a24568340a4fe8ccef2ccc31960fa1fab
githubbato <usijrmichael79@gmail.com>
2026-02-02 19:06:21 +0800

    fix, vercel.json, environments

--- task-management-app/src/environments/environment.development.ts
+++ task-management-app/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl:
+  apiUrl: 
     window.location.hostname === 'localhost'
       ? 'http://localhost:3000/api'
       : 'https://task-management-app-8t3d.vercel.app/api',
--- task-management-app/src/environments/environment.ts
+++ task-management-app/src/environments/environment.ts
@@ -1,8 +1,7 @@
 export const environment = {
-  production: false,
-  apiUrl:
-    window.location.hostname === 'localhost'
-      ? 'http://localhost:3000/api'
-      : 'https://task-management-app-8t3d.vercel.app/api',
+  production: true,
+  apiUrl: window.location.hostname === 'localhost'
+    ? 'http://localhost:3000/api'
+    : 'https://taskdone-production.up.railway.app/api',
   trackingApiUrl: 'https://visitor-tracking-api.vercel.app/api/visit',
 };
